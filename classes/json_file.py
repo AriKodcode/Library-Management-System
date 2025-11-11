d21181e140e1a031325b7e5fc456f3509dbea082
meir_p <meir12345p@gmail.com>
2025-11-11 12:30:19 +0200

    create a json class whit convert_to_dict, wirte ,read methods

---
+++ classes/json_file.py
@@ -0,0 +1,29 @@
+import json
+
+
+class Json_File:
+
+    def convert_to_dict(self, users, books):
+        list_of_users = []
+        list_of_books = []
+
+        for user in users:
+            list_of_users.append(user.info_user_to_dict())
+
+        for book in books:
+            list_of_books.append(book.get_book_info_dict())
+
+        return list_of_books, list_of_users
+
+    def write(self, user, books):
+        books_list, users_list = self.convert_to_dict(user, books)
+        data = {"books": books_list,
+                "users": users_list
+                }
+
+        with open("data.json", "w") as f:
+            f.write(json.dumps(data))
+
+    def read(self):
+        with open("data.json", "r") as f:
+            return json.loads(f.read())
